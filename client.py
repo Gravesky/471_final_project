1d07e230004b237315532086369243ab8957bf36
Huaicheng-D <1651681700@qq.com>
2020-12-02 10:10:48 +0800

    modified server and client program

--- client.py
+++ client.py
@@ -1,24 +1,58 @@
 import socket
 import sys
+import select
+import errno
 
+HDR_LENGTH = 8
+host = 'localhost'
+port = 1989
+
+# Setting the client name
+clientName = input ("Please enter your name for the chatroom: ")
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket creation success at client side")
 except socket.error:
     print("Socket creation failed at client side")
 
-port = 1234
-sock.connect(('localhost', port))
+sock.connect((host, port))
+
+cid = clientName.encode()
+cid_length = len(cid)
+client_hdr = (str(cid_length) + ":<" + str(HDR_LENGTH)).encode()
+sock.send(client_hdr + cid)
 
 print("Welcome to this chat room")
 print("Type words and press enter to send message")
 print("Type quit and press enter to exit the connection")
 
+#while (True):
+    
+#    recv_msg = sock.recv(1024)
+#    print("Server: " + recv_msg.decode())
+#    snd_msg = input()
+#    sock.send( snd_msg.encode())
 while (True):
-    snd_msg = input()
-    sock.send( snd_msg.encode())
-    recv_msg = sock.recv(1024)
+    msg = input(clientName + ":")
+    if (msg == True):
+        msg = msg.encode()
+        msg_len = len(msg)
+        msg_hdr = (str(msg_len) + ":<" + str(HDR_LENGTH)).encode()
+        sock.send(msg_hdr + msg)
 
-    print("Server: " + recv_msg.decode())
+    while True:
+        # Getting the header length and client name
+        recv_header = sock.recv(HDR_LENGTH)
+        if not len(recv_header):
+            sys.exit()
+        recv_length = int(recv_header.decode().strip())
+        recvName = sock.recv(recv_length).decode()
+
+        # Getting the message 
+        msg_header = sock.recv(HDR_LENGTH)
+        msg_length = int(msg_header.decode().strip())
+        message = sock.recv(msg_length).decode()
+
+        print(recvName+":"+message)
 
 sock.close()
--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 import socket
 import sys
+import select
 
+HDR_LENGTH = 8
 host = 'localhost'
-port = 1234
+port = 1989
 
 # Creating Socket at Server Side
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #加了下面这一行可以重复使用地址
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
     print("Socket creation success at server")
 except socket.error:
     print ("Socket creation error at server")
@@ -15,29 +20,99 @@ addr = (host, port)
 sock.bind(addr)
 print("Server socket binded to localhost and port #%s" %(port))
 
-sock.listen(5)
+sock.listen()
 print("Server socket is listening")
+
+#下面这两段code是对应多个client的socket
+sockets_list = [sock]
+
+clients = {} # A list of client info that is stored in dictionary
+
+#收到packet时，先读header，知道msg大小后，继续往后读
+def recv_pkt(client_sock):
+    try:
+
+        pkt_hdr = client_sock.recv(HDR_LENGTH)
+
+        if pkt_hdr is False:
+            print("This client has close the connection.")
+            return False
+
+        payload = client_sock.recv(pkt_hdr)
+        
+        ret = {'HDR' : pkt_hdr, 'data' : payload}
+
+        return ret
+    
+    except:
+        print("This client has close the connection.")
+        return False
 
 while (True):
 
-    conn, address = sock.accept()
-    print("New incoming connection ")
+    # conn, address = sock.accept()
+    # print("New incoming connection ")
 
-    while (True):
+    # while (True):
         
-        recv_msg = conn.recv(1024)
+    #     recv_msg = conn.recv(1024)
 
-        if (recv_msg.decode() == 'quit'):
-            print("Client quit the chat")
-            snd_msg = ("You have quit the chat")
-            conn.send(snd_msg.encode())
-            conn.close()
-            break
+    #     if (recv_msg.decode() == 'quit'):
+    #         print("Client quit the chat")
+    #         snd_msg = ("You have quit the chat")
+    #         conn.send(snd_msg.encode())
+    #         conn.close()
+    #         break
 
-        print("Client: " + recv_msg.decode())
-        snd_msg = input()
-        conn.send(snd_msg.encode())
-    
+    #     print("Client: " + recv_msg.decode())
+    #     snd_msg = input()
+    #     conn.send(snd_msg.encode())
+
+    #下面都是我改（chao）的
+    read_sock = select.select(sockets_list, [], sockets_list)
+
+    for new_info_sock in read_sock:
+        if new_info_sock != sock:
+            #curently recieving packets/msg from one of the client 
+            msg = recv_pkt(new_info_sock)
+
+            if msg is False:
+                print("Client"+ clients[new_info_sock]['data'].decode('utf-8') +"has close the connection.")
+                sockets_list.remove(new_info_sock)
+                del clients[new_info_sock]
+
+                continue
+            
+            user = clients[new_info_sock]
+
+            for client_sock in clients:
+                if client_sock != new_info_sock:
+                    #TODO ENCRYPTION REQUIRED
+                    client_sock.send(user['HDR']+user['data']+msg['HDR']+msg['data'])
+            
+        else:
+            #currently recieving packets from server (new connection)
+            conn, address = sock.accept()
+            
+            user = recv_pkt(conn)
+
+            if user is False:
+                continue
+
+            sockets_list.append(conn)
+
+            clients[conn] = user
+
+            print("New incoming connection at "+str(conn)+" : "+str(*address) + " from "+user['data'].decode('utf-8'))
+
+#以上就是所有改动，直接借鉴吧，因为即使完全借鉴的话，我们最后还是要加入好多的东西（比如加密和UI），并且直接借鉴速度还快一点。
+
+
+
+
+
+
+
 
 
 
