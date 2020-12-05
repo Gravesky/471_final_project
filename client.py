de99920d2d75fc51147936cafd5ff0b1611e820c
Huaicheng-D <1651681700@qq.com>
2020-12-05 16:53:27 +0800

    Encapsulate send and receive to fit tkinter. Implement tkinter AI

--- client.py
+++ client.py
@@ -4,6 +4,8 @@ import select
 import signal
 import time
 import crypt
+import tkinter as tk
+import tkinter.font as ft
 
 HDR_LENGTH = 8
 host = 'localhost'
@@ -72,16 +74,6 @@ while(PrvKnum < 600):
     #print("Private key = "+str(PrvKnum))
     PrvKnum = crypt.generatePK(t,PrvKexp)
 
-print("<<<<<<<<<<<<<<<<<<< Welcome to this chat room >>>>>>>>>>>>>>>>>>>>>>")
-print("<<<<<<<<<<<< Type words and press enter to send message >>>>>>>>>>>>")
-
-#while (True):
-    
-#    recv_msg = sock.recv(1024)
-#    print("Server: " + recv_msg.decode())
-#    snd_msg = input()
-#    sock.send( snd_msg.encode())
-
 #TODO Get the public key from the sender
 PubKexp = -1
 PubKnum = -1
@@ -101,16 +93,46 @@ while(PubKexp == -1 or PubKnum == -1):
             PubKnum = int(sock.recv(PubKnum_size).decode())
             print("[CLIENT] Public Num key is assigned...")
     except BlockingIOError as e:
-        print("ERROR : "+str(e))
+        #print("ERROR : "+str(e))
         time.sleep(5)
 
 print("Recieved public keys are:\n exp = "+str(PubKexp)+" num = "+str(PubKnum))
     
 #print('p = '+str(theNum1)+' q = '+str(theNum2)+' n = '+str(n)+' t = '+str(t)+' e = '+str(PrvKexp)+' d = '+str(PrvKnum))
-while (True):
-        
-    orig_msg = str(input(clientName + ":"))
-    
+
+
+def receive_display():
+    # Display the received message on Chat screen
+    while True:
+        # Getting the header length and client name (From who)
+        try:
+            recv_header = sock.recv(HDR_LENGTH)
+            if not len(recv_header):
+                sys.exit()
+            recv_length = int(recv_header.decode().strip())
+            recvName = sock.recv(recv_length).decode()
+            # Getting the message (Says what)
+            #TODO add decryption
+            msg_header = sock.recv(HDR_LENGTH)
+            msg_length = int(msg_header.decode().strip())
+            crypt_message = sock.recv(msg_length).decode()
+            #print("[CLIENT] Ciphered Message recieved = "+crypt_message)
+            #print("private key = "+str(PrvKnum)+" n = "+str(n))
+            message = crypt.dText(crypt.strToList(crypt_message), PrvKnum, n)
+            msg_box.insert(tk.END, recvName + ": " + message)
+            print(recvName+":"+message)
+
+        # The non-blocking recv() cause IO exception when the client has not typed in any message
+        # If it is the case that recv() is waiting, break    
+        except BlockingIOError:
+            break
+
+
+def send_display(event = None):
+    # Display the sent message on Chat screen
+    orig_msg = tk_msg.get()
+    msg_box.insert(tk.END, clientName + ": " + tk_msg.get())
+
     if orig_msg:
         #TODO add encryption
         #print("exp = "+str(PubKexp))
@@ -121,29 +143,91 @@ while (True):
         msg_hdr = "{ml:<{hl}}".format(ml = msg_len, hl = HDR_LENGTH).encode()
         #msg_hdr = (str(msg_len) + ":<" + str(HDR_LENGTH)).encode()
         sock.send(msg_hdr + msg)
-    try:
-        while True:
-            # Getting the header length and client name (From who)
-            recv_header = sock.recv(HDR_LENGTH)
-            if not len(recv_header):
-                sys.exit()
-            recv_length = int(recv_header.decode().strip())
-            recvName = sock.recv(recv_length).decode()
+    
+    tk_msg.set("")
 
-            # Getting the message (Says what)
-            #TODO add decryption
-            msg_header = sock.recv(HDR_LENGTH)
-            msg_length = int(msg_header.decode().strip())
-            crypt_message = sock.recv(msg_length).decode()
-            #print("[CLIENT] Ciphered Message recieved = "+crypt_message)
-            #print("private key = "+str(PrvKnum)+" n = "+str(n))
-            message = crypt.dText(crypt.strToList(crypt_message), PrvKnum, n)
-                                 
-            print(recvName+":"+message)
+# ========================================== tkinter UI Section ===========================================
 
-    # The non-blocking recv() cause IO exception when the client has not typed in any message
-    # If it is the case that recv() is waiting, continue 
-    except IOError:
-        continue
+# Setting the associated attributes for tkinter
+chat = tk.Tk()
+chat.title("Chat")
+
+msg_frame = tk.Frame(chat)
+scrollbar = tk.Scrollbar(msg_frame)
+
+msg_font = ft.Font(family="Arial",size=12)
+msg_box = tk.Listbox(msg_frame, height = 20, width = 75, yscrollcommand=scrollbar.set, font = msg_font)
+msg_box.pack(side = tk.LEFT, fill = tk.BOTH)
+
+msg_box.pack()
+msg_frame.pack()
+
+tk_msg = tk.StringVar()
+tk_msg.set("Type Your Message Here")
+
+entry_font = ft.Font(family="Arial",size=12,weight="bold")
+input_entry = tk.Entry(chat, width = 50, textvariable = tk_msg, font = entry_font)
+input_entry.bind("<Return>", send_display)
+input_entry.pack()
+
+sendButton = tk.Button(chat, text="Send", command=send_display, bg='darkcyan', fg='snow', width = 25)
+sendButton['font'] = ft.Font(size = 12)
+sendButton.pack(side = 'right')
+
+refreshButton = tk.Button(chat, text = "Refresh", command = receive_display, bg = 'lightslategrey', fg = 'snow', width = 10)
+refreshButton['font'] = ft.Font(size = 12)
+refreshButton.pack(side = 'left')
+
+msg_box.insert(tk.END, "<<<<<<<<<<<<<<<<<<<<<<<<<< Welcome to the Chatroom >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+msg_box.insert(tk.END, "<<<<<<<<<<<<<<<<<<<<< Type message and click 'Send' button >>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+
+chat.mainloop()
+
+
+
+# ===================================== End of Tkinter UI Section =========================================
+
+
+#while (True):
+        
+#    #orig_msg = str(input(clientName + ":"))
+#    orig_msg = tk_msg.get()
+#    #send_display(clientName, orig_msg)
+#    if orig_msg:
+#        #TODO add encryption
+#        #print("exp = "+str(PubKexp))
+#        msg = crypt.listToStr(crypt.cText(orig_msg,PubKexp, PubKnum))
+#        #print("[CLIENT] Ciphered Message sent = "+msg)
+#        msg = msg.encode()
+#        msg_len = len(msg)
+#        msg_hdr = "{ml:<{hl}}".format(ml = msg_len, hl = HDR_LENGTH).encode()
+#        #msg_hdr = (str(msg_len) + ":<" + str(HDR_LENGTH)).encode()
+#        sock.send(msg_hdr + msg)
+#    try:
+#        while True:
+#            # Getting the header length and client name (From who)
+#            recv_header = sock.recv(HDR_LENGTH)
+#            if not len(recv_header):
+#                sys.exit()
+#            recv_length = int(recv_header.decode().strip())
+#            recvName = sock.recv(recv_length).decode()
+
+#            # Getting the message (Says what)
+#            #TODO add decryption
+#            msg_header = sock.recv(HDR_LENGTH)
+#            msg_length = int(msg_header.decode().strip())
+#            crypt_message = sock.recv(msg_length).decode()
+#            #print("[CLIENT] Ciphered Message recieved = "+crypt_message)
+#            #print("private key = "+str(PrvKnum)+" n = "+str(n))
+#            message = crypt.dText(crypt.strToList(crypt_message), PrvKnum, n)
+#            receive_display(recvName, message)                     
+#            print(recvName+":"+message)
+
+#    # The non-blocking recv() cause IO exception when the client has not typed in any message
+#    # If it is the case that recv() is waiting, continue 
+#    except IOError:
+#        continue
+
 
 sock.close()
